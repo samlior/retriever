5f240025288d7ea978440796b2e2330d0c5f8599
samlior <samlior@foxmail.com>
2021-02-28 17:09:54 +0800

    chore: update pack.js

--- pack.js
+++ pack.js
@@ -5,55 +5,42 @@ async function bundleElectronApp(options) {
   const appPaths = await packager(options)
 }
 
-const appName = "Retriever"
+const appName = 'Retriever'
 
 async function main() {
     
-    let platform = ""
-    let arch = ""
-    let out = ""
+    let platform = 'win32'
+    let arch = 'x64'
+    let out = 'out-win'
     
-    try {
-        if (process.argv[2] === "darwin") {
-            platform = "darwin"
-            arch = "x64"
-            out = "out-darwin-asar"
-        }
-        else {
-            platform = "win32"
-            arch = "x64"
-            out = "out-win-asar"
-        }
-    }
-    catch(e) {
-        console.log("use default params")
-        platform = "win32"
-        arch = "x64"
-        out = "out-win-asar"
+    if (process.argv[2] === 'darwin') {
+        platform = 'darwin'
+        arch = 'x64'
+        out = 'out-darwin'
     }
 
     await bundleElectronApp({
-        dir: ".",
+        dir: '.',
         appVersion: settings.version,
         arch: arch,
         asar: false,
         ignore: (fileName)=>{
-            if (fileName === "") {
+            if (fileName === '') {
                 return false
             }
-            if (fileName.indexOf(".bin") !== -1) {
+            if (fileName.indexOf('.bin') !== -1) {
                 return true
             }
             for (let n of settings.packFileName) {
                 if (fileName.indexOf(n) === 0 || n.indexOf(fileName) === 0) {
-                    console.log("pack:" + fileName)
+                    console.log('pack:' + fileName)
                     return false
                 }
             }
-            console.log("ignore:" + fileName)
+            console.log('ignore:' + fileName)
             return true
         },
-        electronZipDir: "./zip",
+        electronZipDir: './zip',
         name: appName,
         out: out,
         overwrite: true,
